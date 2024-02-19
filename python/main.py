c3e22404bd1f34d979714fcd4cf7e57f95a63245
haduki uchiyama <uchiyama.hazuki7@gmail.com>
2024-02-19 23:24:07 +0900

    3-4

--- python/main.py
+++ python/main.py
@@ -4,13 +4,13 @@ import pathlib
 import json
 import hashlib
 import shutil
-from fastapi import FastAPI, Form, HTTPException, UploadFile
+from fastapi import FastAPI, Form, HTTPException, UploadFile, File
 from fastapi.responses import FileResponse
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 logger = logging.getLogger("uvicorn")
-logger.level = logging.INFO
+logger.level = logging.DEBUG
 images = pathlib.Path(__file__).parent.resolve() / "images"
 origins = [os.environ.get("FRONT_URL", "http://localhost:3000")]
 app.add_middleware(
@@ -27,9 +27,15 @@ def root():
     return {"message": "Hello, world!"}
 
 @app.post("/items")
-def add_item(name: str = Form(...), category: str = Form(...)):
-    logger.info(f"Receive item: {name} {category}")
-    new_item = {"name": name, "category": category}
+def add_item(name: str = Form(...), category: str = Form(...), image: UploadFile = File(...)):
+    images_folder = "images"
+    image_content = image.file.read()
+    image_hash = hashlib.sha256(image_content).hexdigest() + ".jpg"
+    image_path = os.path.join(images_folder, image_hash)
+    with open(image_path, "wb") as image_file:
+        image_file.write(image_content)
+    logger.info(f"Receive item: {name} {category} {image_hash}")
+    new_item = {"name": name, "category": category, "image_name":image_hash}
     with open('items.json', 'r') as file:
         items_data = json.load(file)
     items_data["items"].append(new_item)
@@ -43,6 +49,13 @@ def get_items():
         items_data = json.load(file)
     return items_data
 
+@app.get("/items/{item_id}")
+def get_item_id(item_id: int):
+    with open('items.json', 'r') as file:
+        items_data = json.load(file)
+        items_list = items_data["items"]
+        item = items_list[item_id]
+        return item
 
 @app.get("/image/{image_name}")
 async def get_image(image_name):
